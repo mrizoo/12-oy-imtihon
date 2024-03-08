43806a53536a771a0767914abfd7459999978272
mukhammadrizo <pulatovvmukhammadrizo@gmail.com>
2024-03-09 04:27:46 +0500

    js imtihon

---
+++ app.js
@@ -0,0 +1,47 @@
+//🚩
+// N2✅
+// let item = 15;
+// function getSum3(item) {
+//   let result = 0;
+//   for (let i = 3; i <= item; i += 3) {
+//     result += i;
+//   }
+//   return result
+// }
+// console.log(getSum3(item));
+
+
+// N3✅
+// let num = 5; 
+// function result(son) {
+//     let yigindi = 0;
+//     for (let i = 1; i <= son; i++) {
+//         yigindi += i;
+//     }
+//     return yigindi;
+// }
+// console.log(result(num));
+
+
+
+//N4✅
+// let text = [
+//     "Abdulaziz",
+//     "Safarmurod",
+//     "Muhammadrizo",
+//     "Ilyos"];
+//     let result = {};
+//     for (let i = 0; i < text.length; i++) {
+//   let key = text[i];
+//   let value = key.length;
+//   result[key] = value;
+//   console.log(result);
+// }
+
+
+// N7✅
+// const text = "Men Abdulaziz Programmerman".split(' ').some((prabel) =>{
+//    return prabel.trim
+// })
+// console.log(text);
+
